293869347330b002ce5102c037f64da0a3c746da
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-12-25 01:39:31 +0100

    implemented JavaScript eval

--- src/gui_manual_vuln_verification.py
+++ src/gui_manual_vuln_verification.py
@@ -1,12 +1,27 @@
 import tkinter as tk
+import dukpy
 import sys
 import os
 
 
 def main():
-    def on_button_click():
+    def on_button_click(_event):
         # Example button functionality
         print("Button clicked")
+
+    def eval_js(_event):
+        js_input = js_input_text.get("1.0", tk.END)
+        print(f"JS input: {js_input}")
+        try:
+            js_output = dukpy.evaljs(js_input)
+        except Exception as e:
+            js_output = f"Error: {e}"
+        print(f"JS output: {js_output}")
+        js_output_text.config(state=tk.NORMAL)
+        js_output_text.delete("1.0", tk.END)
+        js_output_text.insert(tk.END, str(js_output))
+        js_output_text.config(state=tk.DISABLED)
+        return "break"  # returning "break" prevents the ENTER press from appending a newline char to the text!
 
     root = tk.Tk()
     root.title("Manual vulnerability verification GUI")
@@ -53,9 +68,10 @@ def main():
     file_content_text = tk.Text(root, state="disabled")
     file_content_text.grid(row=1, column=2, rowspan=3, sticky="nsew", padx=5, pady=5)
 
-    tk.Label(root, text="JavaScript console:", anchor="w").grid(row=4, column=2, sticky="ew", padx=5, pady=5)
+    tk.Label(root, text="JavaScript eval:", anchor="w").grid(row=4, column=2, sticky="ew", padx=5, pady=5)
     js_input_text = tk.Text(root, height=1)
     js_input_text.grid(row=5, column=2, sticky="nsew", padx=5, pady=5)
+    js_input_text.bind("<Return>", eval_js)
 
     js_output_text = tk.Text(root, height=1, state="disabled")
     js_output_text.grid(row=6, column=2, sticky="nsew", padx=5, pady=5)
