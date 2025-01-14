b76c5c466bb7bc1be0e8460b6a95527bb063f8d5
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2025-01-14 18:44:04 +0100

    result_csv_stats.py: now using argparse, new optional --list-vuln-ext argument to list all vulnerable extensions in alphabetical order, more helpful error message if CSV line has too few commas

--- src/result_csv_stats.py
+++ src/result_csv_stats.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import os
 import statistics
@@ -15,14 +16,17 @@ def longest_common_prefix(a: str, b: str) -> str:
     return common_prefix
 
 
-def print_result_csv_stats(result_csv_path: str):
+def print_result_csv_stats(result_csv_path: str,
+                           list_vuln_ext: bool):
     no_ext: int = 0
     no_vuln_ext: int = 0
     no_vuln_ext_exploitable: int = 0
     no_dangers: int = 0
     no_dangers_exploitable: int = 0
     analysis_times: List[float] = []
+    analysis_times_vuln_ext: List[float] = []
     extension_common_prefix: Optional[str] = None
+    vulnerable_extensions: List[str] = []
 
     with open(result_csv_path) as csv_file:
         # Determine the relevant column indices using the CSV header line:
@@ -34,10 +38,14 @@ def print_result_csv_stats(result_csv_path: str):
 
         for line in csv_file:
             line_split: List[str] = line.split(",")
-            extension: str = line_split[col_idx_extension]
-            cs_injected_into: List[str] = line_split[col_idx_cs_injected_into].split(" | ")
-            analysis_time: float = float(line_split[col_idx_analysis_time])
-            total_dangers: int = int(line_split[col_idx_total_dangers])
+            try:
+                extension: str = line_split[col_idx_extension]
+                cs_injected_into: List[str] = line_split[col_idx_cs_injected_into].split(" | ")
+                analysis_time: float = float(line_split[col_idx_analysis_time])
+                total_dangers: int = int(line_split[col_idx_total_dangers])
+            except IndexError:
+                print(f"CSV error: Line has too few commas: {line_split}")
+                exit(1)
 
             if extension_common_prefix is None:
                 extension_common_prefix = extension
@@ -48,6 +56,8 @@ def print_result_csv_stats(result_csv_path: str):
             no_dangers += total_dangers
             if total_dangers > 0:
                 no_vuln_ext += 1
+                vulnerable_extensions.append(extension)
+                analysis_times_vuln_ext.append(analysis_time)
             analysis_times.append(analysis_time)
 
             exploitable: bool = any(is_an_injected_everywhere_url_pattern(url_pattern)
@@ -66,21 +76,38 @@ def print_result_csv_stats(result_csv_path: str):
     print(f"No. of vulnerable extensions: {no_vuln_ext} ({no_vuln_ext_exploitable} exploitable)")
     print(f"No. of vulnerabilities/dangers found: {no_dangers} ({no_dangers_exploitable} exploitable)")
     print(f"Median analysis time: {statistics.median(analysis_times)}")
+    print(f"Median analysis time of vulnerable extensions: {statistics.median(analysis_times_vuln_ext)}")
     print("")
+
+    if list_vuln_ext:
+        vulnerable_extensions.sort()
+        print(f"Vulnerable extensions (count: {len(vulnerable_extensions)}):")
+        for vuln_ext in vulnerable_extensions:
+            print(vuln_ext)
+        print("")
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 result_csv_stats.py <RESULT_CSV_FILE> ...")
-        exit(1)
-    else:
-        for result_csv_path in sys.argv[1:]:
-            if os.path.isfile(result_csv_path):
-                print_result_csv_stats(result_csv_path=result_csv_path)
-            else:
-                print()
-                print(f"Error: {result_csv_path} is not a file!")
-                print()
+    parser = argparse.ArgumentParser(prog='result_csv_stats',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     description="Reads a result.csv file and prints statistics.")
+
+    parser.add_argument('RESULT_CSV_FILE', nargs='+')
+
+    parser.add_argument("--list-vuln-ext", dest='list_vuln_ext', action='store_true',
+                        help="List all vulnerable extensions in alphabetical order.")
+
+    args = parser.parse_args()
+
+    for result_csv_path in args.RESULT_CSV_FILE:
+        if os.path.isfile(result_csv_path):
+            print_result_csv_stats(result_csv_path=result_csv_path,
+                                   list_vuln_ext=args.list_vuln_ext,
+                                   )
+        else:
+            print()
+            print(f"Error: {result_csv_path} is not a file!")
+            print()
 
 
 if __name__ == "__main__":
