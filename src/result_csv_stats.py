5e9351f6246eca71a97f5cb8a24e989f35d441f4
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2025-03-24 21:05:19 +0100

    result_csv_stats.py: now also printing info by vulnerability type (BP exfiltration / BP infiltration / CS infiltration)

--- src/result_csv_stats.py
+++ src/result_csv_stats.py
@@ -19,11 +19,30 @@ def longest_common_prefix(a: str, b: str) -> str:
 def print_result_csv_stats(result_csv_path: str,
                            list_vuln_ext: bool,
                            list_vuln_exploitable_ext: bool):
-    no_ext: int = 0
-    no_vuln_ext: int = 0
+    no_ext: int = 0  # (number of extensions)
+    no_vuln_ext: int = 0  # (number of vulnerable extensions)
     no_vuln_ext_exploitable: int = 0
     no_dangers: int = 0
     no_dangers_exploitable: int = 0
+    # ----- -----
+    bp_exf_no_ext: int = 0
+    bp_exf_no_dangers: int = 0
+    bp_exf_no_ext_exploitable: int = 0
+    bp_exf_no_dangers_exploitable: int = 0
+    bp_inf_no_ext: int = 0
+    bp_inf_no_dangers: int = 0
+    bp_inf_no_ext_exploitable: int = 0
+    bp_inf_no_dangers_exploitable: int = 0
+    # ---
+    cs_exf_no_ext: int = 0
+    cs_exf_no_dangers: int = 0
+    cs_exf_no_ext_exploitable: int = 0
+    cs_exf_no_dangers_exploitable: int = 0
+    cs_inf_no_ext: int = 0
+    cs_inf_no_dangers: int = 0
+    cs_inf_no_ext_exploitable: int = 0
+    cs_inf_no_dangers_exploitable: int = 0
+    # ----- -----
     analysis_times: List[float] = []
     analysis_times_vuln_ext: List[float] = []
     extension_common_prefix: Optional[str] = None
@@ -37,6 +56,11 @@ def print_result_csv_stats(result_csv_path: str,
         col_idx_cs_injected_into: int = header_line.index("CS injected into")
         col_idx_analysis_time: int = header_line.index("analysis time in seconds")
         col_idx_total_dangers: int = header_line.index("total dangers")
+        # -----
+        col_idx_bp_exfiltration_dangers: int = header_line.index("BP exfiltration dangers")
+        col_idx_bp_infiltration_dangers: int = header_line.index("BP infiltration dangers")
+        col_idx_cs_exfiltration_dangers: int = header_line.index("CS exfiltration dangers")
+        col_idx_cs_infiltration_dangers: int = header_line.index("CS infiltration dangers")
 
         for line in csv_file:
             line_split: List[str] = line.split(",")
@@ -45,6 +69,11 @@ def print_result_csv_stats(result_csv_path: str,
                 cs_injected_into: List[str] = line_split[col_idx_cs_injected_into].split(" | ")
                 analysis_time: float = float(line_split[col_idx_analysis_time])
                 total_dangers: int = int(line_split[col_idx_total_dangers])
+                # -----
+                no_bp_exfiltration_dangers: int = int(line_split[col_idx_bp_exfiltration_dangers])
+                no_bp_infiltration_dangers: int = int(line_split[col_idx_bp_infiltration_dangers])
+                no_cs_exfiltration_dangers: int = int(line_split[col_idx_cs_exfiltration_dangers])
+                no_cs_infiltration_dangers: int = int(line_split[col_idx_cs_infiltration_dangers])
             except IndexError:
                 print(f"CSV error: Line has too few commas: {line_split}")
                 exit(1)
@@ -71,6 +100,42 @@ def print_result_csv_stats(result_csv_path: str,
                     no_vuln_ext_exploitable += 1
                     vulnerable_exploitable_extensions.append(extension)
 
+            # BP exfiltration dangers:
+            bp_exf_no_dangers += no_bp_exfiltration_dangers
+            if no_bp_exfiltration_dangers > 0:
+                bp_exf_no_ext += 1
+            if exploitable:
+                bp_exf_no_dangers_exploitable += no_bp_exfiltration_dangers
+                if no_bp_exfiltration_dangers > 0:
+                    bp_exf_no_ext_exploitable += 1
+
+            # BP infiltration dangers:
+            bp_inf_no_dangers += no_bp_infiltration_dangers
+            if no_bp_infiltration_dangers > 0:
+                bp_inf_no_ext += 1
+            if exploitable:
+                bp_inf_no_dangers_exploitable += no_bp_infiltration_dangers
+                if no_bp_infiltration_dangers > 0:
+                    bp_inf_no_ext_exploitable += 1
+
+            # CS exfiltration dangers:
+            cs_exf_no_dangers += no_cs_exfiltration_dangers
+            if no_cs_exfiltration_dangers > 0:
+                cs_exf_no_ext += 1
+            if exploitable:
+                cs_exf_no_dangers_exploitable += no_cs_exfiltration_dangers
+                if no_cs_exfiltration_dangers > 0:
+                    cs_exf_no_ext_exploitable += 1
+
+            # CS infiltration dangers:
+            cs_inf_no_dangers += no_cs_infiltration_dangers
+            if no_cs_infiltration_dangers > 0:
+                cs_inf_no_ext += 1
+            if exploitable:
+                cs_inf_no_dangers_exploitable += no_cs_infiltration_dangers
+                if no_cs_infiltration_dangers > 0:
+                    cs_inf_no_ext_exploitable += 1
+
     print("")
     print("***** result.csv stats: *****")
     print(f"Result.csv file: {result_csv_path}")
@@ -78,6 +143,12 @@ def print_result_csv_stats(result_csv_path: str,
     print(f"No. of extensions: {no_ext}")
     print(f"No. of vulnerable extensions: {no_vuln_ext} ({no_vuln_ext_exploitable} exploitable)")
     print(f"No. of vulnerabilities/dangers found: {no_dangers} ({no_dangers_exploitable} exploitable)")
+    print("-----")
+    print(f"BP exfiltration: {bp_exf_no_dangers} dangers in {bp_exf_no_ext} extensions (exploitable: {bp_exf_no_dangers_exploitable} dangers in {bp_exf_no_ext_exploitable} extensions)")
+    print(f"BP UXSS/infiltration: {bp_inf_no_dangers} dangers in {bp_inf_no_ext} extensions (exploitable: {bp_inf_no_dangers_exploitable} dangers in {bp_inf_no_ext_exploitable} extensions)")
+    print(f"CS exfiltration: {cs_exf_no_dangers} dangers in {cs_exf_no_ext} extensions (exploitable: {cs_exf_no_dangers_exploitable} dangers in {cs_exf_no_ext_exploitable} extensions)")
+    print(f"CS UXSS/infiltration: {cs_inf_no_dangers} dangers in {cs_inf_no_ext} extensions (exploitable: {cs_inf_no_dangers_exploitable} dangers in {cs_inf_no_ext_exploitable} extensions)")
+    print("-----")
     print(f"Median analysis time: {statistics.median(analysis_times)}")
     print(f"Median analysis time of vulnerable extensions: {statistics.median(analysis_times_vuln_ext)}")
     print("")
