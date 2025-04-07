cb7dea47c76bd5c4a76ba60b7c28f9d826042795
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2025-04-07 23:39:09 +0200

    result_csv_stats.py: implemented --combine-csvs cmd line flag; todo: test

--- src/result_csv_stats.py
+++ src/result_csv_stats.py
@@ -16,10 +16,10 @@ def longest_common_prefix(a: str, b: str) -> str:
     return common_prefix
 
 
-def print_result_csv_stats(result_csv_path: str,
+def print_result_csv_stats(result_csv_paths: List[str],
                            list_vuln_ext: bool,
                            list_vuln_exploitable_ext: bool,
-                           timeout_in_sec: int):
+                           timeouts_in_sec: List[int]):
     no_ext: int = 0  # (number of extensions)
     no_vuln_ext: int = 0  # (number of vulnerable extensions)
     no_vuln_ext_exploitable: int = 0  # (number of vulnerable, exploitable extensions)
@@ -49,99 +49,107 @@ def print_result_csv_stats(result_csv_path: str,
     extension_common_prefix: Optional[str] = None
     vulnerable_extensions: List[str] = []
     vulnerable_exploitable_extensions: List[str] = []
+    # ----- -----
+    no_of_timeouts: int = 0
 
-    with open(result_csv_path) as csv_file:
-        # Determine the relevant column indices using the CSV header line:
-        header_line: List[str] = next(csv_file).split(",")
-        col_idx_extension: int = header_line.index("Extension")  # = the path to the .CRX file on the original machine
-        col_idx_cs_injected_into: int = header_line.index("CS injected into")
-        col_idx_analysis_time: int = header_line.index("analysis time in seconds")
-        col_idx_total_dangers: int = header_line.index("total dangers")
-        # -----
-        col_idx_bp_exfiltration_dangers: int = header_line.index("BP exfiltration dangers")
-        col_idx_bp_infiltration_dangers: int = header_line.index("BP infiltration dangers")
-        col_idx_cs_exfiltration_dangers: int = header_line.index("CS exfiltration dangers")
-        col_idx_cs_infiltration_dangers: int = header_line.index("CS infiltration dangers")
+    for index, result_csv_path in enumerate(result_csv_paths):
+        with open(result_csv_path) as csv_file:
+            # Determine the relevant column indices using the CSV header line:
+            header_line: List[str] = next(csv_file).split(",")
+            col_idx_extension: int = header_line.index("Extension")  # = the path to the .CRX file on the original machine
+            col_idx_cs_injected_into: int = header_line.index("CS injected into")
+            col_idx_analysis_time: int = header_line.index("analysis time in seconds")
+            col_idx_total_dangers: int = header_line.index("total dangers")
+            # -----
+            col_idx_bp_exfiltration_dangers: int = header_line.index("BP exfiltration dangers")
+            col_idx_bp_infiltration_dangers: int = header_line.index("BP infiltration dangers")
+            col_idx_cs_exfiltration_dangers: int = header_line.index("CS exfiltration dangers")
+            col_idx_cs_infiltration_dangers: int = header_line.index("CS infiltration dangers")
 
-        for line in csv_file:
-            line_split: List[str] = line.split(",")
-            try:
-                extension: str = line_split[col_idx_extension]
-                cs_injected_into: List[str] = line_split[col_idx_cs_injected_into].split(" | ")
-                analysis_time: float = float(line_split[col_idx_analysis_time])
-                total_dangers: int = int(line_split[col_idx_total_dangers])
-                # -----
-                def parse_int(s: str) -> int:
-                    return 0 if s == "N/A" else int(s)
-                no_bp_exfiltration_dangers: int = parse_int(line_split[col_idx_bp_exfiltration_dangers])
-                no_bp_infiltration_dangers: int = parse_int(line_split[col_idx_bp_infiltration_dangers])
-                no_cs_exfiltration_dangers: int = parse_int(line_split[col_idx_cs_exfiltration_dangers])
-                no_cs_infiltration_dangers: int = parse_int(line_split[col_idx_cs_infiltration_dangers])
-            except IndexError:
-                print(f"CSV error: Line has too few commas: {line_split}")
-                exit(1)
+            for line in csv_file:
+                line_split: List[str] = line.split(",")
+                try:
+                    extension: str = line_split[col_idx_extension]
+                    cs_injected_into: List[str] = line_split[col_idx_cs_injected_into].split(" | ")
+                    analysis_time: float = float(line_split[col_idx_analysis_time])
+                    total_dangers: int = int(line_split[col_idx_total_dangers])
+                    # -----
+                    def parse_int(s: str) -> int:
+                        return 0 if s == "N/A" else int(s)
+                    no_bp_exfiltration_dangers: int = parse_int(line_split[col_idx_bp_exfiltration_dangers])
+                    no_bp_infiltration_dangers: int = parse_int(line_split[col_idx_bp_infiltration_dangers])
+                    no_cs_exfiltration_dangers: int = parse_int(line_split[col_idx_cs_exfiltration_dangers])
+                    no_cs_infiltration_dangers: int = parse_int(line_split[col_idx_cs_infiltration_dangers])
+                except IndexError:
+                    print(f"CSV error: Line has too few commas: {line_split}")
+                    exit(1)
 
-            if extension_common_prefix is None:
-                extension_common_prefix = extension
-            else:
-                extension_common_prefix = longest_common_prefix(extension_common_prefix, extension)
+                if extension_common_prefix is None:
+                    extension_common_prefix = extension
+                else:
+                    extension_common_prefix = longest_common_prefix(extension_common_prefix, extension)
 
-            no_ext += 1
-            no_dangers += total_dangers
-            if total_dangers > 0:
-                no_vuln_ext += 1
-                vulnerable_extensions.append(extension)
-                analysis_times_vuln_ext.append(analysis_time)
-            analysis_times.append(analysis_time)
-
-            exploitable: bool = any(is_an_injected_everywhere_url_pattern(url_pattern)
-                                    for url_pattern in cs_injected_into)
-
-            if exploitable:
-                no_dangers_exploitable += total_dangers
+                no_ext += 1
+                no_dangers += total_dangers
                 if total_dangers > 0:
-                    no_vuln_ext_exploitable += 1
-                    vulnerable_exploitable_extensions.append(extension)
+                    no_vuln_ext += 1
+                    vulnerable_extensions.append(extension)
+                    analysis_times_vuln_ext.append(analysis_time)
+                analysis_times.append(analysis_time)
 
-            # BP exfiltration dangers:
-            bp_exf_no_dangers += no_bp_exfiltration_dangers
-            if no_bp_exfiltration_dangers > 0:
-                bp_exf_no_ext += 1
-            if exploitable:
-                bp_exf_no_dangers_exploitable += no_bp_exfiltration_dangers
+                # Increment no_of_timeouts by 1 if analysis_time >= timeout:
+                timeout: int = timeouts_in_sec[index] if index < len(timeouts_in_sec) else timeouts_in_sec[-1]
+                if analysis_time >= timeout:
+                    no_of_timeouts += 1
+
+                exploitable: bool = any(is_an_injected_everywhere_url_pattern(url_pattern)
+                                        for url_pattern in cs_injected_into)
+
+                if exploitable:
+                    no_dangers_exploitable += total_dangers
+                    if total_dangers > 0:
+                        no_vuln_ext_exploitable += 1
+                        vulnerable_exploitable_extensions.append(extension)
+
+                # BP exfiltration dangers:
+                bp_exf_no_dangers += no_bp_exfiltration_dangers
                 if no_bp_exfiltration_dangers > 0:
-                    bp_exf_no_ext_exploitable += 1
+                    bp_exf_no_ext += 1
+                if exploitable:
+                    bp_exf_no_dangers_exploitable += no_bp_exfiltration_dangers
+                    if no_bp_exfiltration_dangers > 0:
+                        bp_exf_no_ext_exploitable += 1
 
-            # BP infiltration dangers:
-            bp_inf_no_dangers += no_bp_infiltration_dangers
-            if no_bp_infiltration_dangers > 0:
-                bp_inf_no_ext += 1
-            if exploitable:
-                bp_inf_no_dangers_exploitable += no_bp_infiltration_dangers
+                # BP infiltration dangers:
+                bp_inf_no_dangers += no_bp_infiltration_dangers
                 if no_bp_infiltration_dangers > 0:
-                    bp_inf_no_ext_exploitable += 1
+                    bp_inf_no_ext += 1
+                if exploitable:
+                    bp_inf_no_dangers_exploitable += no_bp_infiltration_dangers
+                    if no_bp_infiltration_dangers > 0:
+                        bp_inf_no_ext_exploitable += 1
 
-            # CS exfiltration dangers:
-            cs_exf_no_dangers += no_cs_exfiltration_dangers
-            if no_cs_exfiltration_dangers > 0:
-                cs_exf_no_ext += 1
-            if exploitable:
-                cs_exf_no_dangers_exploitable += no_cs_exfiltration_dangers
+                # CS exfiltration dangers:
+                cs_exf_no_dangers += no_cs_exfiltration_dangers
                 if no_cs_exfiltration_dangers > 0:
-                    cs_exf_no_ext_exploitable += 1
+                    cs_exf_no_ext += 1
+                if exploitable:
+                    cs_exf_no_dangers_exploitable += no_cs_exfiltration_dangers
+                    if no_cs_exfiltration_dangers > 0:
+                        cs_exf_no_ext_exploitable += 1
 
-            # CS infiltration dangers:
-            cs_inf_no_dangers += no_cs_infiltration_dangers
-            if no_cs_infiltration_dangers > 0:
-                cs_inf_no_ext += 1
-            if exploitable:
-                cs_inf_no_dangers_exploitable += no_cs_infiltration_dangers
+                # CS infiltration dangers:
+                cs_inf_no_dangers += no_cs_infiltration_dangers
                 if no_cs_infiltration_dangers > 0:
-                    cs_inf_no_ext_exploitable += 1
+                    cs_inf_no_ext += 1
+                if exploitable:
+                    cs_inf_no_dangers_exploitable += no_cs_infiltration_dangers
+                    if no_cs_infiltration_dangers > 0:
+                        cs_inf_no_ext_exploitable += 1
 
     print("")
     print("***** result.csv stats: *****")
-    print(f"Result.csv file: {result_csv_path}")
+    print(f"Result.csv file(s): {result_csv_paths}")
     print(f"Extension common prefix: {extension_common_prefix}")
     print(f"No. of extensions: {no_ext}")
     print(f"No. of vulnerable extensions: {no_vuln_ext} ({no_vuln_ext_exploitable} exploitable)")
@@ -154,8 +162,7 @@ def print_result_csv_stats(result_csv_path: str,
     print("-----")
     print(f"Median analysis time: {statistics.median(analysis_times)}")
     print(f"Median analysis time of vulnerable extensions: {statistics.median(analysis_times_vuln_ext)}")
-    no_of_timeouts: int = len([t for t in analysis_times if t >= timeout_in_sec])
-    print(f"Timeouts (ext. with analysis time >= {timeout_in_sec}sec): {no_of_timeouts} ({(100*no_of_timeouts)/no_ext:.2f}%)")
+    print(f"Timeouts: {no_of_timeouts} ({(100*no_of_timeouts)/no_ext:.2f}%) (timeouts used: {timeouts_in_sec})")
     print("-----")  # 60%, 70%, 80% and 90% quantiles:
     analysis_times.sort()
     for q in [0.6, 0.7, 0.8, 0.9]:
@@ -191,29 +198,46 @@ def main():
                         action='store_true',
                         help="List all vulnerable exploitable(!) extensions in alphabetical order.")
 
-    parser.add_argument("--timeout", metavar="SECONDS", type=int, default=3600,
-                        help="The timeout (in seconds) that was used for generating the given CSV file. "
-                             "Default: 3600 (i.e., 1 hour)")
+    parser.add_argument("--timeout", metavar="SECONDS", type=str, default="3600",
+                        help="The timeout (in seconds) that was used for generating the given CSV file(s). "
+                             "If multiple CSV files are given, multiple timeouts may be given, separated with commas. "
+                             "If fewer timeouts are given than CSV files, the last timeout listed will be "
+                             "used for all remaining CSV files. "
+                             "Default: 3600 (i.e., 1 hour, for all CSV files given)")
 
-    # TODO:
-    # parser.add_argument("--combine-csvs", dest='combine_csvs',
-    #                     action='store_true',
-    #                     help="Combine all CSV files given as arguments and generate statistics on them as a whole. "
-    #                          "Stills prints individual stats in tabular form.")
+    parser.add_argument("--combine-csvs", dest='combine_csvs',
+                        action='store_true',
+                        help="Combine all CSV files given as arguments and generate statistics on them as a whole. "
+                             "This flag has no effect when only 1 CSV file is given.")
+    # TODO: still print stats for each INDIVIDUAL CSV file in tabular form?!
 
     args = parser.parse_args()
 
-    for result_csv_path in args.RESULT_CSV_FILE:
-        if os.path.isfile(result_csv_path):
-            print_result_csv_stats(result_csv_path=result_csv_path,
-                                   list_vuln_ext=args.list_vuln_ext,
-                                   list_vuln_exploitable_ext=args.list_vuln_exploitable_ext,
-                                   timeout_in_sec=args.timeout
-                                   )
-        else:
-            print()
-            print(f"Error: {result_csv_path} is not a file!")
-            print()
+    # --combine-csvs flag was set:
+    if args.combine_csvs:
+        for result_csv_path in args.RESULT_CSV_FILE:
+            if not os.path.isfile(result_csv_path):
+                print()
+                print(f"Error: {result_csv_path} is not a file!")
+                print()
+
+        print_result_csv_stats(result_csv_paths=args.RESULT_CSV_FILE,
+                               list_vuln_ext=args.list_vuln_ext,
+                               list_vuln_exploitable_ext=args.list_vuln_exploitable_ext,
+                               timeouts_in_sec=[int(i) for i in args.timeout.split(",")])
+
+    # --combine-csvs flag was NOT set, generate N individual statistics:
+    else:
+        for result_csv_path in args.RESULT_CSV_FILE:
+            if os.path.isfile(result_csv_path):
+                print_result_csv_stats(result_csv_paths=[result_csv_path],
+                                       list_vuln_ext=args.list_vuln_ext,
+                                       list_vuln_exploitable_ext=args.list_vuln_exploitable_ext,
+                                       timeouts_in_sec=[int(i) for i in args.timeout.split(",")])
+            else:
+                print()
+                print(f"Error: {result_csv_path} is not a file!")
+                print()
 
 
 if __name__ == "__main__":
